3500b312dcce26a5a4e228a574bf91cd11a628ea
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-07-22 18:09:41 -0400

    roughing out

---
+++ db.js
@@ -0,0 +1,46 @@
+var categories = {
+  'Test1 Category': [
+    {
+      name: 'Test Red',
+      id: 1
+    },
+    {
+      name: 'Test Blue',
+      id: 2
+    }
+  ],
+  'Test2 Category': [
+    {
+      name: 'Test Car',
+      id: 1
+    },
+    {
+      name: 'Test Boat',
+      id: 2
+    }
+  ]
+};
+
+module.exports = {
+  getCategoryNames: () => {
+
+  },
+  getProductsByCategory: () => {
+
+  },
+  createProduct: () => {
+
+  },
+  deleteProduct: () => {
+
+  },
+  updateProduct: () => {
+
+  },
+  deleteCategory: () => {
+
+  },
+  createCategory: () => {
+
+  }
+};
---
+++ routes/categories.js
@@ -0,0 +1,24 @@
+const db = require('../db');
+const router = require('express').Router();
+
+module.exports = router;
+
+router.get('/:name/products', (req, res, next) => {
+
+});
+
+router.post('/', (req, res, next) => {
+
+});
+
+router.delete('/:name', (req, res, next) => {
+
+});
+
+router.post('/:name/products', (req, res, next) => {
+
+});
+
+router.delete('/:name/products/:id', (req, res, next) => {
+
+});
